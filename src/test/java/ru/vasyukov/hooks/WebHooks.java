b589ea38992cd723f25d2e85bca6aa124bb94428
awa <and3081@yandex.ru>
2022-07-16 00:54:34 +0300

    README.md

--- src/test/java/ru/vasyukov/hooks/WebHooks.java
+++ src/test/java/ru/vasyukov/hooks/WebHooks.java
@@ -5,7 +5,6 @@ import custom.properties.TestData;
 import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.remote.DesiredCapabilities;
